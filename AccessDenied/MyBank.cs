c1c38c5c42056da5c0d331d67939477ffa1f47f8
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-06-26 13:26:13 +0200

    feat:add MyBank class

---
+++ AccessDenied/MyBank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDenied
+{
+    //a class with internal access modifier
+    internal class MyBank
+    {
+         // a public property
+        public string Name { get; set; }
+
+        //protected int AccountNumber { get; set; }  
+        internal int AccountNumber { get; set; }
+        public int Balance { get; set;}
+        internal void CreditPrompt() {
+            Console.WriteLine($"Dear {Name}, you have {Balance} FrinkCoins in your account {AccountNumber}.");
+        }  
+    }
+}
---
+++ AccessDenied/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AccessDenied
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MyBank customerA = new MyBank();
+            customerA.Name = "Homer Simpson";  
+            customerA.Balance = 250;
+            //if protected access modifier is used for the property AccountNumber, it could not be access at Program.cs, however if it is changed to public or internal it could be accessed
+            customerA.AccountNumber = 101;
+            customerA.CreditPrompt();
+        }
+    }
+}
